bf632d491bcb0a3cd77e89c3aab6a901af1dc8d2
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-11 07:36:52 +0100

    Initial commit and added method for calculating total cost of fruitbox

---
+++ Sandbox/FruitBox.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    // This class represents a simple box of fruit - it can only contain
+    // Bananas and Apples. It also represents the price of each item. 
+    class FruitBox
+    {
+        // Instance fields
+        private int numberOfBananas;
+        private int numberOfApples;
+        private double priceForOneBanana;
+        private double priceForOneApple;
+
+        // Create the fruit box - it is empty to start with, but the prices must
+        // be specified
+        //Constructor
+        public FruitBox(double priceForOneBanana, double priceForOneApple)
+        {
+            this.priceForOneBanana = priceForOneBanana;
+            this.priceForOneApple = priceForOneApple;
+            this.numberOfBananas = 0;
+            this.numberOfApples = 0;
+        }
+
+        // Mutators
+        public void AddApples(int numberOfApples)
+        {
+            this.numberOfApples = this.numberOfApples + numberOfApples;
+        }
+
+        public void AddBananas(int numberOfBananas)
+        {
+            this.numberOfBananas = this.numberOfBananas + numberOfBananas;
+        }
+
+        // Note that this method just returns a string describing the box content.
+        // It does NOT print anything on the screen!
+        public string GetBoxContentDescription()
+        {
+            string description = "The box contains " + numberOfBananas + " bananas and " + numberOfApples + " apples";
+            return description;
+        }
+
+        public double GetTotalPrice()
+        {
+            double totalPrice = (numberOfApples * priceForOneApple) + (numberOfBananas * priceForOneBanana);
+            return totalPrice;
+        }
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            FruitBox f1 = new FruitBox(2.95, 1.5);
+
+            Console.WriteLine(f1.GetBoxContentDescription());
+
+            f1.AddApples(12);
+            f1.AddBananas(7);
+
+            Console.WriteLine(f1.GetBoxContentDescription());
+            Console.WriteLine("The total price of the fruitbox is {0} kr", f1.GetTotalPrice());
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
